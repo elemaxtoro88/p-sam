0515b8edf78104a6fc39c2f9fc11765cf1af670a
elemaxtoro88 <elemaxtoro@gmail.com>
2026-06-09 15:45:56 -0300

    Switch to Gemini v1 API

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post('/api/analyze', (req, res) => {
     }
 
     const model = 'gemini-1.5-flash';
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
+    const url = `https://generativelanguage.googleapis.com/v1/models/${model}:generateContent?key=${apiKey}`;
 
     const data = JSON.stringify(req.body);
 
